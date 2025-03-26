2847f9bdd7902ae4697430a2984fdac3476604fe
Owen Coughlan <owenc1993@gmail.com>
2025-03-26 15:38:36 +0000

    completed code and downloaded cso.json file

--- assignments/assignment03-cso.py
+++ assignments/assignment03-cso.py
@@ -1,3 +1,7 @@
 import requests
 
 url = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en'
+response = requests.get(url)
+with open('cso.json', 'wb') as file:
+    file.write(response.content)
+print('File downloaded')
